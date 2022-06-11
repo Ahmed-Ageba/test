560b1be61752e70b50db4715a2cfb54187c2f57e
Ahmed-Ageba <97427078+Ahmed-Ageba@users.noreply.github.com>
2022-06-11 09:04:55 +0200

    Update File.py

--- File.py
+++ File.py
@@ -1,1 +1,43 @@
-echo "Holaaaaaaaaaaaa"
+# comment1 
+# comment2 python 
+# -------------------------------
+from multiprocessing.dummy import JoinableQueue
+from platform import python_branch
+
+
+print(type(10)) # int =>Integer
+print(type(100)) # int =>Integer
+print(type(-50)) # int =>Integer
+
+print(type(100.9)) # float =>Floating Point Number
+print(type(1.9435)) # float =>Floating Point Number
+print(type(-10.9665)) # float =>Floating Point Number
+
+print("Hello python") # string => String
+
+print([1, 2, 3, 4, 5]) # list => List
+print(type((1, 2, 3, 4, 5))) # tuple =>Tuple
+print(type({ "one": 1, "two": 2, "three": 3})) #dict =>dictionary
+print(2 == 2) # bool =>Boolean
+name = "Ahmed Ageba" 
+myName = "Ahmed Ageba"
+my_name = "Ahmed Ageba"
+
+print(name)
+print(myName)
+print(my_name)
+age = "27 years old"
+print(age)
+title = "System & Network Administrator"
+print(title)
+ahmed = "27"
+print (ahmed)
+# new lesson
+name = input("enter your name: ")
+
+
+#########################
+num1 = input("enter your no. ")
+num2 = input("etner your second no. ")
+result = num1+num2
+print(result)
